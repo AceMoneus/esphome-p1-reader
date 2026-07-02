844d27cb15b14d01102e23d063109154b7849391
AceMoneus <kjell@moneus.se>
2026-07-02 13:01:39 +0200

    Refactor P1 message parsing and sensor publishing

--- esphome/components/p1_reader/p1_reader.cpp
+++ esphome/components/p1_reader/p1_reader.cpp
@@ -13,11 +13,50 @@ void P1Reader::loop() {
 
     buffer_.push_back(c);
 
+    // End of message (P1 telegrams end with newline)
     if (c == '\n') {
       ParsedMessage msg;
-      if (msg.parse(buffer_, this->protocol_ == "hdlc")) {
+
+      bool is_hdlc = (this->protocol_ == "hdlc");
+      if (msg.parse(buffer_, is_hdlc)) {
         ESP_LOGD(TAG, "Parsed P1 message");
+
+        // Publish sensors if values exist
+        if (energy_import_sensor_ && msg.energy_import.has_value())
+          energy_import_sensor_->publish_state(*msg.energy_import);
+
+        if (power_import_sensor_ && msg.power_import.has_value())
+          power_import_sensor_->publish_state(*msg.power_import);
+
+        if (voltage_l1_sensor_ && msg.voltage_l1.has_value())
+          voltage_l1_sensor_->publish_state(*msg.voltage_l1);
+
+        if (voltage_l2_sensor_ && msg.voltage_l2.has_value())
+          voltage_l2_sensor_->publish_state(*msg.voltage_l2);
+
+        if (voltage_l3_sensor_ && msg.voltage_l3.has_value())
+          voltage_l3_sensor_->publish_state(*msg.voltage_l3);
+
+        if (current_l1_sensor_ && msg.current_l1.has_value())
+          current_l1_sensor_->publish_state(*msg.current_l1);
+
+        if (current_l2_sensor_ && msg.current_l2.has_value())
+          current_l2_sensor_->publish_state(*msg.current_l2);
+
+        if (current_l3_sensor_ && msg.current_l3.has_value())
+          current_l3_sensor_->publish_state(*msg.current_l3);
+
+        if (power_l1_sensor_ && msg.power_l1.has_value())
+          power_l1_sensor_->publish_state(*msg.power_l1);
+
+        if (power_l2_sensor_ && msg.power_l2.has_value())
+          power_l2_sensor_->publish_state(*msg.power_l2);
+
+        if (power_l3_sensor_ && msg.power_l3.has_value())
+          power_l3_sensor_->publish_state(*msg.power_l3);
       }
+
+      // Clear buffer for next telegram
       buffer_.clear();
     }
   }
